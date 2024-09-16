5aa0424dfd3936d260807e359ac2691c5ecbccdb
Taseen <n7165095@gmail.com>
2024-09-16 14:50:32 +0500

    taseen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function Counter() {
-  const [count, setCount] = useState(0);
- 
-
+function App() {
   return (
-    <div>
-      <p>Current Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default Counter;
+export default App;
